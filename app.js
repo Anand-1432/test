eeec2937ec53fc90bea66fd7d1522b32d10fbe82
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-10-14 22:12:07 +0530

    port1

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect("mongodb+srv://anand:anand1432@cluster0.q2rn7.mongodb.net/sptrade?retryWrites=true&w=majority").then(()=>{
+    console.log("connection established....!");
+}).catch((error)=>{
+    console.log(error);
+})
 
 app.get("/",(req,res)=>{
     res.send("hellow from other side");
